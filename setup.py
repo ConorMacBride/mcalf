c21190f2b545bcbaefbd7062cde214afdf1dbc68
Conor MacBride <conor@macbride.me>
2021-01-15 22:09:28 +0000

    Moved setup configuration to setup.cfg
    Also changes dependencies: removed docutils (unused); min version 1.3->1.4 scipy (quad_vec not in 1.3.*); min version 0.21->0.22 (py38 support added 0.22).
    
    Signed-off-by: Conor MacBride <conor@macbride.me>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
 from distutils.command.build_ext import build_ext
 
 
@@ -34,43 +34,9 @@ class CTypes(Extension):
     pass
 
 
-def read(file_name):
-    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
-
-
 is_nt = '_nt' if os.name == 'nt' else ''
 
 setup(
-    name="mcalf",
-    version="0.1.1",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-
-    python_requires=">=3.6",
-    install_requires=["docutils>=0.3", "numpy>=1.17", "scipy>=1.3",
-                      "pyyaml>=5.1", "pathos>=0.2.5", "scikit-learn>=0.21",
-                      "matplotlib>=3.1", "astropy>=3.2", "pytest", "pytest-cov"],
-
     ext_modules=[CTypes("mcalf.profiles.ext_voigtlib", ["cextern/voigt{}.c".format(is_nt)])],
     cmdclass={'build_ext': build_ext},
-
-    author="Conor MacBride",
-    author_email="cmacbride01@qub.ac.uk",
-    license="BSD 2-Clause",
-    description="MCALF: Multi-Component Atmospheric Line Fitting",
-    keywords="spectrum spectra fitting absorption emission voigt",
-    url="https://github.com/ConorMacBride/mcalf/",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: C",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering :: Astronomy",
-        "Topic :: Scientific/Engineering :: Physics"
-    ],
-    long_description=read('README.rst')
 )
