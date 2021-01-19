30732bbd1b1dfab0dc6faff4829daf34016bd195
Conor MacBride <conor@macbride.me>
2021-01-19 19:01:43 +0000

    Try finding package data using pkg_resources
    Signed-off-by: Conor MacBride <conor@macbride.me>

--- src/mcalf/tests/utils/test_misc.py
+++ src/mcalf/tests/utils/test_misc.py
@@ -1,9 +1,14 @@
 import pytest
 import os
+import pkg_resources
 import numpy as np
 from astropy.io import fits
 
 from mcalf.utils.misc import make_iter, load_parameter, merge_results
+
+
+def data_path(*args, module='utils'):
+    return pkg_resources.resource_filename('mcalf', os.path.join('tests', module, 'data', *args))
 
 
 def test_make_iter():
@@ -25,39 +30,36 @@ def test_load_parameter_string():
 
 def test_load_parameter_file():
 
-    abspath = f"{os.path.dirname(os.path.abspath(__file__))}{os.path.sep}data{os.path.sep}"
     truth = np.array([243.23, 62, 2523.43, 0, -62.2, np.inf, np.nan], dtype=np.float64)
 
     # Unsupported extension given
     with pytest.raises(ValueError):
-        load_parameter(f"{abspath}test_load_parameter_file.unsupported")
+        load_parameter(data_path('test_load_parameter_file.unsupported'))
 
     # Test each file type
     for ext in ['npy', 'csv', 'fits', 'sav']:
-        res = load_parameter(f"{abspath}test_load_parameter_file.{ext}")
+        res = load_parameter(data_path(f'test_load_parameter_file.{ext}'))
         assert res[:-1] == pytest.approx(truth[:-1])
         assert np.isnan(res[-1])
 
     # Test CSV with defined wl
-    res = load_parameter(f"{abspath}test_load_parameter_file_wl.csv", wl=2523.43)
+    res = load_parameter(data_path('test_load_parameter_file_wl.csv'), wl=2523.43)
     assert res[:-1] == pytest.approx(truth[:-1])
     assert np.isnan(res[-1])
 
     # Test CSV with expected exceptions
     for e in ['typeerror', 'syntaxerror']:
         with pytest.raises(SyntaxError):  # Note: the TypeError is converted into a SyntaxError
-            res = load_parameter(f"{abspath}test_load_parameter_file_{e}.csv", wl=2523.43)
+            res = load_parameter(data_path(f'test_load_parameter_file_{e}.csv'), wl=2523.43)
 
 
 def test_merge_results(tmp_path):
 
-    abspath = f"{os.path.dirname(os.path.abspath(__file__))}{os.path.sep}data{os.path.sep}"
-
     # Compatible files to test merging
     compatible_files = [
-        abspath + "test_merge_results_1.fits",
-        abspath + "test_merge_results_2.fits",
-        abspath + "test_merge_results_3.fits",
+        data_path('test_merge_results_1.fits'),
+        data_path('test_merge_results_2.fits'),
+        data_path('test_merge_results_3.fits'),
     ]
 
     # Merge and save
@@ -66,17 +68,17 @@ def test_merge_results(tmp_path):
 
     # Compare merged files to expected merge
     test = fits.open(output_file, mode='readonly')
-    verify = fits.open(abspath + "test_merge_results_all.fits", mode='readonly')
+    verify = fits.open(data_path('test_merge_results_all.fits'), mode='readonly')
     # Diff ignoring checksums as too strict (compare values instead)
     diff = fits.FITSDiff(test, verify, ignore_keywords=['CHECKSUM', 'DATASUM'])
     assert diff.identical  # If this fails tolerances *may* need to be adjusted
 
     # Incompatible files to test merging
     incompatible_files = [
-        abspath + "test_merge_results_1.fits",
-        abspath + "test_merge_results_2.fits",
-        abspath + "test_merge_results_3.fits",
-        abspath + "test_merge_results_2.fits",  # Duplicate (overlapping) file should fail
+        data_path('test_merge_results_1.fits'),
+        data_path('test_merge_results_2.fits'),
+        data_path('test_merge_results_3.fits'),
+        data_path('test_merge_results_2.fits'),  # Duplicate (overlapping) file should fail
     ]
 
     # Merge (should fail before saving)
@@ -85,9 +87,9 @@ def test_merge_results(tmp_path):
 
     # Compatible files but wrong time for one (should fail)
     compatible_files_wrongtime = [
-        abspath + "test_merge_results_1.fits",
-        abspath + "test_merge_results_2_wrongtime.fits",
-        abspath + "test_merge_results_3.fits",
+        data_path('test_merge_results_1.fits'),
+        data_path('test_merge_results_2_wrongtime.fits'),
+        data_path('test_merge_results_3.fits'),
     ]
 
     # Merge (should fail before saving)
@@ -96,21 +98,21 @@ def test_merge_results(tmp_path):
 
     # Must provide a list of multiple files
     with pytest.raises(TypeError):  # single string
-        merge_results(abspath + "test_merge_results_1.fits", output_file)
+        merge_results(data_path('test_merge_results_1.fits'), output_file)
     with pytest.raises(TypeError):  # list of length 1
-        merge_results([abspath + "test_merge_results_1.fits"], output_file)
+        merge_results([data_path('test_merge_results_1.fits')], output_file)
 
     # The extra HDU should cause an error
     with pytest.raises(ValueError) as excinfo:
         merge_results([
-            abspath + "test_merge_results_2.fits",
-            abspath + "test_merge_results_1_extrahdu.fits",
+            data_path('test_merge_results_2.fits'),
+            data_path('test_merge_results_1_extrahdu.fits'),
         ], output_file)
     assert 'nexpected' in str(excinfo.value)  # "Unexpected"
     # reverse (now extra is in first file)
     with pytest.raises(ValueError) as excinfo:
         merge_results([
-            abspath + "test_merge_results_1_extrahdu.fits",
-            abspath + "test_merge_results_2.fits",
+            data_path('test_merge_results_1_extrahdu.fits'),
+            data_path('test_merge_results_2.fits'),
         ], output_file)
     assert 'nexpected' in str(excinfo.value)  # "Unexpected"
