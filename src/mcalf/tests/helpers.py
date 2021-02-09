f2004e3d570f5e7806b7db066917f93bca06455f
Conor MacBride <conor@macbride.me>
2021-02-09 17:01:14 +0000

    Add tests and pytest-mpl
    Signed-off-by: Conor MacBride <conor@macbride.me>

--- src/mcalf/tests/helpers.py
+++ src/mcalf/tests/helpers.py
@@ -1,8 +1,14 @@
 import os
 import pkg_resources
+from pathlib import Path
+from functools import wraps
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pytest
 
 
-__all__ = ['data_path_function']
+__all__ = ['data_path_function', 'get_hash_library_name', 'figure_test']
 
 
 def data_path_function(mod):
@@ -10,3 +16,49 @@ def data_path_function(mod):
     def data_path(*args, module=mod):
         return pkg_resources.resource_filename('mcalf', os.path.join('tests', module, 'data', *args))
     return data_path
+
+
+def get_hash_library_name():
+    """
+    Generate the hash library name for this env.
+    Notes
+    -----
+    Based on functions at https://github.com/sunpy/sunpy/blob/v2.0.7/sunpy/tests/helpers.py
+    The SunPy Community et al. SunPy (Version v2.0.7). Zenodo. http://doi.org/10.5281/zenodo.4423217
+    """
+    ft2_version = f"{mpl.ft2font.__freetype_version__.replace('.', '')}"
+    mpl_version = "dev" if "+" in mpl.__version__ else mpl.__version__.replace('.', '')
+    return f"figure_hashes_mpl_{mpl_version}_ft_{ft2_version}.json"
+
+
+def figure_test(test_function):
+    """
+    A decorator for a test that verifies the hash of the current figure or the
+    returned figure, with the name of the test function as the hash identifier
+    in the library. A PNG is also created in the 'result_image' directory,
+    which is created on the current path.
+    All such decorated tests are marked with `pytest.mark.figure` for convenient filtering.
+    Examples
+    --------
+    @figure_test
+    def test_simple_plot():
+        plt.plot([0,1])
+    Notes
+    -----
+    Based on functions at https://github.com/sunpy/sunpy/blob/v2.0.7/sunpy/tests/helpers.py
+    The SunPy Community et al. SunPy (Version v2.0.7). Zenodo. http://doi.org/10.5281/zenodo.4423217
+    """
+    hash_library_name = get_hash_library_name()
+    hash_library_file = Path(__file__).parent / hash_library_name
+
+    @pytest.mark.mpl_image_compare(hash_library=hash_library_file,
+                                   savefig_kwargs={'metadata': {'Software': None}},
+                                   style='default')
+    @wraps(test_function)
+    def test_wrapper(*args, **kwargs):
+        ret = test_function(*args, **kwargs)
+        if ret is None:
+            ret = plt.gcf()
+        return ret
+
+    return test_wrapper
---
+++ src/mcalf/tests/visualisation/test_spec.py
@@ -0,0 +1,38 @@
+import pytest
+import numpy as np
+
+from mcalf.visualisation.spec import plot_spectrum
+from ..helpers import figure_test
+
+
+@pytest.fixture(scope='function')
+def wavelengths_spectrum():
+    wavelengths = np.linspace(-1, 1, 30)
+    wavelengths = np.delete(wavelengths, [1, 3, 5, 7, -8, -6, -4, -2])
+    spectrum = -4.523 * np.exp(-wavelengths ** 2 / 0.05) + 11.354
+    wavelengths += 2432.249
+    return wavelengths, spectrum
+
+
+@figure_test
+def test_plot_spectrum(wavelengths_spectrum):
+    wavelengths, spectrum = wavelengths_spectrum
+    plot_spectrum(wavelengths, spectrum)
+
+
+@figure_test
+def test_plot_spectrum_no_smooth(wavelengths_spectrum):
+    wavelengths, spectrum = wavelengths_spectrum
+    plot_spectrum(wavelengths, spectrum, smooth=False)
+
+
+@figure_test
+def test_plot_spectrum_no_norm(wavelengths_spectrum):
+    wavelengths, spectrum = wavelengths_spectrum
+    plot_spectrum(wavelengths, spectrum, normalised=False)
+
+
+@figure_test
+def test_plot_spectrum_no_smooth_norm(wavelengths_spectrum):
+    wavelengths, spectrum = wavelengths_spectrum
+    plot_spectrum(wavelengths, spectrum, smooth=False, normalised=False)
